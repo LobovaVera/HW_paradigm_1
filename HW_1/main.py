c35ef5cb6beada56cb76fe898582de8f01aa4553
Vera <pchilli@yandex.ru>
2023-09-28 17:57:02 +0300

    reverse added

--- HW_1/main.py
+++ HW_1/main.py
@@ -5,9 +5,10 @@ def main():
     numbers_1 = [ 1,4,6,7,8,0,2,1,6,3,9]
     numbers_i = sort_1.sort_list_imperative(numbers_1)
     print("императивный ")
-    print(numbers_i)
+    print(sort_1.reverse(numbers_i))
     
-    numbers_2 = [ 1,4,6,7,8,0,2,1,6,3,9]
+    
+    numbers_2 = [ 1,4,6,7,8,0,2,1,6,3,9,11]
     numbers_i2 = sort_2.sort_list_declarative(numbers_2)
     print("декларативный")
     print(numbers_i2)
--- HW_1/sort_1.py
+++ HW_1/sort_1.py
@@ -1,10 +1,23 @@
 #puzirkom
 def sort_list_imperative(numbers: list):
     for i in range(len(numbers)-1, 0, -1):
-        for j in range( 0, i):
+        for j in range(0, i):
             if numbers[j]>numbers[j+1]:
                 temp = numbers[j]
                 numbers[j] = numbers[j+1]
                 numbers[j+1] = temp
     
     return numbers
+
+def reverse(numbers: list):
+    k = len(numbers)/2
+    for i in range(len(numbers) - 1, int(k)-1, -1):
+        temp = numbers[len(numbers)-i]
+        numbers[len(numbers)-1-i] = numbers[i] 
+        numbers[i] = temp
+    return numbers
+
+
+numbers_2 = [ 0, 1, 1, 2, 3, 4, 8]
+print(reverse(numbers_2))
+print(numbers_2)
